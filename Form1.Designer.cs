7d88a64b7c00473661c84ca505a228db1472981d
ghiegie <giancarlogarcia.gallano@gmail.com>
2023-12-18 16:40:50 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,397 @@
+﻿namespace CustomerModule {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            checkBox1 = new CheckBox();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            textBox6 = new TextBox();
+            label8 = new Label();
+            textBox7 = new TextBox();
+            label9 = new Label();
+            textBox8 = new TextBox();
+            groupBox2 = new GroupBox();
+            textBox9 = new TextBox();
+            label10 = new Label();
+            textBox10 = new TextBox();
+            label11 = new Label();
+            textBox11 = new TextBox();
+            label12 = new Label();
+            textBox12 = new TextBox();
+            label13 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Customer Form";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox8);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(textBox7);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Location = new Point(12, 70);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(469, 254);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Organization Details";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(21, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Customer ID:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(103, 40);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(360, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 34);
+            label3.Name = "label3";
+            label3.Size = new Size(113, 15);
+            label3.TabIndex = 0;
+            label3.Text = "Organization Name:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(125, 31);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(338, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 60);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 15);
+            label4.TabIndex = 2;
+            label4.Text = "Address:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(6, 78);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(457, 23);
+            textBox3.TabIndex = 3;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(6, 116);
+            label5.Name = "label5";
+            label5.Size = new Size(97, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Delivery Address:";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(109, 115);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(137, 19);
+            checkBox1.TabIndex = 5;
+            checkBox1.Text = "Same as the Address:";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(6, 134);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(457, 23);
+            textBox4.TabIndex = 6;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(9, 181);
+            label6.Name = "label6";
+            label6.Size = new Size(29, 15);
+            label6.TabIndex = 7;
+            label6.Text = "TIN:";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(54, 178);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(114, 23);
+            textBox5.TabIndex = 8;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(9, 211);
+            label7.Name = "label7";
+            label7.Size = new Size(39, 15);
+            label7.TabIndex = 9;
+            label7.Text = "Email:";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(54, 208);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(114, 23);
+            textBox6.TabIndex = 10;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(174, 181);
+            label8.Name = "label8";
+            label8.Size = new Size(112, 15);
+            label8.TabIndex = 11;
+            label8.Text = "Telephone Number:";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(292, 178);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(171, 23);
+            textBox7.TabIndex = 12;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(174, 211);
+            label9.Name = "label9";
+            label9.Size = new Size(111, 15);
+            label9.TabIndex = 13;
+            label9.Text = "Cellphone Number:";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(292, 208);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(171, 23);
+            textBox8.TabIndex = 14;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox9);
+            groupBox2.Controls.Add(label10);
+            groupBox2.Controls.Add(textBox10);
+            groupBox2.Controls.Add(label11);
+            groupBox2.Controls.Add(textBox11);
+            groupBox2.Controls.Add(label12);
+            groupBox2.Controls.Add(textBox12);
+            groupBox2.Controls.Add(label13);
+            groupBox2.Location = new Point(12, 341);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(469, 100);
+            groupBox2.TabIndex = 4;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Contact Person";
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(290, 54);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(171, 23);
+            textBox9.TabIndex = 22;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(172, 57);
+            label10.Name = "label10";
+            label10.Size = new Size(111, 15);
+            label10.TabIndex = 21;
+            label10.Text = "Cellphone Number:";
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(290, 24);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(171, 23);
+            textBox10.TabIndex = 20;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(172, 27);
+            label11.Name = "label11";
+            label11.Size = new Size(112, 15);
+            label11.TabIndex = 19;
+            label11.Text = "Telephone Number:";
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(52, 54);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(114, 23);
+            textBox11.TabIndex = 18;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(7, 57);
+            label12.Name = "label12";
+            label12.Size = new Size(39, 15);
+            label12.TabIndex = 17;
+            label12.Text = "Email:";
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(52, 24);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(114, 23);
+            textBox12.TabIndex = 16;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(7, 27);
+            label13.Name = "label13";
+            label13.Size = new Size(42, 15);
+            label13.TabIndex = 15;
+            label13.Text = "Name:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(103, 463);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 5;
+            button1.Text = "Clear";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(186, 463);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 6;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(267, 463);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 7;
+            button3.Text = "Create";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(493, 506);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label6;
+        private TextBox textBox4;
+        private CheckBox checkBox1;
+        private Label label5;
+        private TextBox textBox3;
+        private TextBox textBox8;
+        private Label label9;
+        private TextBox textBox7;
+        private Label label8;
+        private TextBox textBox6;
+        private Label label7;
+        private TextBox textBox5;
+        private GroupBox groupBox2;
+        private TextBox textBox9;
+        private Label label10;
+        private TextBox textBox10;
+        private Label label11;
+        private TextBox textBox11;
+        private Label label12;
+        private TextBox textBox12;
+        private Label label13;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,7 @@
+namespace CustomerModule {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+    }
+}
